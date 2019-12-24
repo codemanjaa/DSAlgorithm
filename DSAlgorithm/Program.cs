db1ca2255c7f3e7fc1a9280506cfb3cc015a89cd
Codemanja <a1600618@myy.haaga-helia.fi>
2019-12-24 21:54:39 +0100

    Create Stack methods

--- DSAlgorithm/Program.cs
+++ DSAlgorithm/Program.cs
@@ -10,6 +10,29 @@ namespace DSAlgorithm
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Stack Loading");
+            Console.Write("Enter the size of the stack: ");
+            int size =  Convert.ToInt32(Console.ReadLine());
+            if(size > 0)
+            {
+                Stack stack = new Stack(size);
+
+                for(int i=0; i<size; i++)
+                {
+                    Console.WriteLine(size);
+                    Console.Write("Enter the numweric value to the Stack: ");
+                    long c = Convert.ToInt64(Console.ReadLine());
+                    stack.push(c);
+                    
+                 
+                }
+
+            }
+
+
+            Console.ReadKey();
         }
+
+       
     }
 }
---
+++ DSAlgorithm/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgorithm
+{
+    public class Stack
+    {
+        private int top;
+        private int maxSize;
+        private long[] stackArray;
+
+        public Stack(int size)
+        {
+            this.maxSize = size;
+            top = -1;
+            stackArray = new long[maxSize];
+        }
+
+        public void push(long element)
+        {
+            top++;
+            stackArray[top] = element;
+        }
+
+        public long pop()
+        {
+            int old_top = top;
+            top--;
+            return stackArray[top];
+        }
+
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+
+
+        public bool isFull()
+        {
+            return (maxSize - 1 == top);
+        }
+    }
+}
