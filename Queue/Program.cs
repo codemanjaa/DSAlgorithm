1f24078fba92ffed544f5a9c4eb667b02c54f99c
Codemanja <a1600618@myy.haaga-helia.fi>
2019-12-27 14:11:24 +0100

    Create Queue algo

---
+++ Queue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queue is loading...");
+            Console.Write("Enter the size of the Queue: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Queue queue = new Queue(size);
+            for(int i=0; i<size; i++)
+            {
+                Console.Write("Enter the long value: ");
+                queue.insert(Convert.ToInt64(Console.ReadLine()));
+
+            }
+            queue.insert(100);
+            queue.view();
+            queue.remove();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Queue
+    {
+        private int size;
+        private int front;
+        private int last;
+        private long[] queueArray;
+        private int nItems;
+
+        public Queue(int size)
+        {
+            queueArray = new long[size];
+            this.size = size;
+            nItems = 0;
+            front = 0;
+            last = -1;
+
+        }
+
+
+        public void insert(long element){
+
+            if (isFull())
+            {
+                Console.WriteLine("Queue is Full...");
+            }
+            else { 
+            last++;
+            queueArray[last] = element;
+            nItems++;
+            }
+        }
+
+        public long remove()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Queue is EMPTY...");
+                return -1;
+            }
+            else { 
+            long temp = queueArray[front];
+            nItems--;
+
+            return temp;
+            }
+        }
+
+        public void view()
+        {
+            Console.Write("[ ");
+            for(int i=0; i< queueArray.Length; i++)
+            {
+                Console.Write(queueArray[i] + " ");
+            }
+            Console.Write("]");
+        }
+
+        public  bool isEmpty()
+        {
+            return (nItems == 0);
+        }
+
+        public bool isFull()
+        {
+            return (size == nItems);
+        }
+    }
+}
